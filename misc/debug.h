cbddb8966455177dd900ff0ad72c07ec05e93c9f
Nathan Yeo <nyeo700@gmail.com>
2026-01-04 22:09:32 +0800

    Added debug.h and short template

---
+++ misc/debug.h
@@ -0,0 +1,209 @@
+#ifdef LOCAL 
+#include<iostream>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& pair);
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec);
+
+template <typename T, size_t SZ>
+std::ostream& operator<<(std::ostream& out, const std::array<T, SZ>& arr);
+
+template <typename T, typename C, typename A>
+std::ostream& operator<<(std::ostream& out, const std::set<T, C, A>& set);
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::multiset<T>& set);
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& map);
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
+    if (vec.empty()) {
+        out << "[]";
+        return out;
+    }
+    out << '[';
+    for (int i = 0; i < vec.size() - 1; i++) {
+        out << vec[i] << ", ";
+    }
+    return out << vec.back() << ']';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<std::vector<T>>& mat) {
+    if (mat.empty()) {
+        out << "[]";
+        return out;
+    }
+    out << "[\n";
+    for (const auto& row : mat) {
+        out << "  " << row << '\n';
+    }
+    return out << "]";
+}
+
+template <typename T, size_t SZ>
+std::ostream& operator<<(std::ostream& out, const std::array<T, SZ>& arr) {
+    if (SZ == 0) {
+        out << "[]";
+        return out;
+    }
+    out << '[';
+    for (int i = 0; i < SZ - 1; i++) {
+        out << arr[i] << ", ";
+    }
+    return out << arr.back() << ']';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::deque<T>& deq) {
+    if (deq.empty()) {
+        out << "[]";
+        return out;
+    }
+    out << '[';
+    for (int i = 0; i < deq.size() - 1; i++) {
+        out << deq[i] << ", ";
+    }
+    return out << deq.back() << ']';
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::unordered_map<T1, T2>& map) {
+    out << '{';
+    for (auto it = map.begin(); it != map.end(); it++) {
+        std::pair<T1, T2> element = *it;
+        out << element.first << ": " << element.second;
+        if (std::next(it) != map.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& map) {
+    out << '{';
+    for (auto it = map.begin(); it != map.end(); it++) {
+        std::pair<T1, T2> element = *it;
+        out << element.first << ": " << element.second;
+        if (std::next(it) != map.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::unordered_set<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::multiset<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::unordered_multiset<T>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+template <typename T, typename C, typename A>
+std::ostream& operator<<(std::ostream& out, const std::set<T, C, A>& set) {
+    out << '{';
+    for (auto it = set.begin(); it != set.end(); it++) {
+        T element = *it;
+        out << element;
+        if (std::next(it) != set.end()) {
+            out << ", ";
+        }
+    }
+    return out << '}';
+}
+
+// Source: https://stackoverflow.com/a/31116392/12128483
+template <typename Type, unsigned N, unsigned Last>
+struct TuplePrinter {
+    static void print(std::ostream& out, const Type& value) {
+        out << std::get<N>(value) << ", ";
+        TuplePrinter<Type, N + 1, Last>::print(out, value);
+    }
+};
+
+template <typename Type, unsigned N>
+struct TuplePrinter<Type, N, N> {
+    static void print(std::ostream& out, const Type& value) {
+        out << std::get<N>(value);
+    }
+};
+
+template <typename... Types>
+std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
+    out << '(';
+    TuplePrinter<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
+    return out << ')';
+}
+
+
+inline void _debug_end() {
+    std::cerr << '\n';
+}
+
+template <typename T>
+inline void _debug_print(const char* name, T&& value) {
+    std::cerr << name << " = " << value;
+}
+
+template <typename T, typename... Args>
+inline void _debug_print(const char* names, T&& value, Args&&... args) {
+    const char* comma = strchr(names, ',');
+    std::cerr.write(names, comma - names) << " = " << value << " | ";
+    _debug_print(comma + 1, std::forward<Args>(args)...);
+}
+
+
+#define debug(...) _debug_print(#__VA_ARGS__, __VA_ARGS__), _debug_end()
+
+#else
+#define debug(x)  // does nothing when LOCAL not defined
+#endif
